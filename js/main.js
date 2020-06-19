874b81e60fd9911b2e5ac9776ba8ee2d79444146
Andrey Ustinov <andrew.ustinov23@gmail.com>
2020-06-19 14:16:14 +0300

    add js news slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 $(document).ready(function () {
-  const mMenuBtn = $('.m-menu-button');
-  const mMenu = $('.m-menu');
-  const body = $('body');
+  /*=== Mobile menu button ===*/
+  const mMenuBtn = $(".m-menu-button");
+  const mMenu = $(".m-menu");
+  const body = $("body");
 
-  mMenuBtn.on('click', function () {
-    mMenu.toggleClass('active');
-    mMenuBtn.toggleClass('active');
-    body.toggleClass('no-scroll');
-  })
+  mMenuBtn.on("click", function () {
+    mMenu.toggleClass("active");
+    mMenuBtn.toggleClass("active");
+    body.toggleClass("no-scroll");
+  });
+
+  /*=== News slider ===*/
+  var mySwiper = new Swiper(".swiper-container", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 4,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: ".button-next",
+        },
+      },
+    },
+  });
 });
